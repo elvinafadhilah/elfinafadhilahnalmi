093ed98125e85a4b60bcab1bb5a39078495ce22a
elvinafadhilah <elvinafadhilah4@gmail.com>
2026-03-05 18:49:57 +0700

    Add files via upload

---
+++ Outputvariabel.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo_2501091021;
+
+/**
+ *
+ * @author LAB-SI-PC
+ */
+public class Outputvariabel 
+{
+    public static void main(String[] args){
+        int value = 10;
+        char x;
+        x = 'A';
+        
+        System.out.println(value);
+        System.out.println("The value of x=" +x);
+    }
+}
