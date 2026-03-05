2a0c315ea9662b1459861dbf267d246b60534b7e
elvinafadhilah <elvinafadhilah4@gmail.com>
2026-03-05 18:49:08 +0700

    Add files via upload

---
+++ AritmatikaDemo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo_2501091021;
+
+/**
+ *
+ * @author LAB-SI-PC
+ */
+public class AritmatikaDemo 
+{
+  public static void main(String[] args) {
+      //sedikit angka
+      int I = 37;
+      int J = 42;
+      double x = 27.475;
+      double Y = 7.22;
+      System.out.println("Variable value...");
+      System.out.println("    i = " + I);
+      System.out.println("    j = " + J);
+      System.out.println("    x = " + x);
+        System.out.println("    y = " + Y);
+  }
+}
