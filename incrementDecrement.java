d344cc32061d8733eb5f4f936a0b388adcb51e0a
elvinafadhilah <elvinafadhilah4@gmail.com>
2026-03-05 18:49:42 +0700

    Add files via upload

---
+++ incrementDecrement.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo_2501091021;
+
+/**
+ *
+ * @author LAB-SI-PC
+ */
+public class incrementDecrement 
+{
+     public static void main(String[] args)
+         {
+            int i = 10;
+            int j = 3;
+            int k = 0;
+
+            k = ++j + i; //akan menghasilkan k = 4+10 =14
+            System.out.println("Variable value...");
+              System.out.println("k =" + k);
+         }
+}
